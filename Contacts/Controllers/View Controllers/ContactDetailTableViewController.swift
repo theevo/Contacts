38bb6efabe9ef15c1be3b82d707269b15e382d5b
Theo Vora <theo.vora@gmail.com>
2020-04-03 11:02:56 -0600

    add draft view controllers and basic storyboard

---
+++ Contacts/Controllers/View Controllers/ContactDetailTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ContactDetailTableViewController.swift
+//  Contacts
+//
+//  Created by Theo Vora on 4/3/20.
+//  Copyright © 2020 Theo Vora. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailTableViewController: UITableViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    
+
+    
+    
+
+}
